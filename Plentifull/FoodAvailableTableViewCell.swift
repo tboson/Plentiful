35b1a567a88548e1bdf74e85aa9f33a8cb1c38b4
tboson <satrfx@gmail.com>
2018-03-26 01:03:09 -0700

    Added need help detail scene and table view

---
+++ Plentifull/FoodAvailableTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodAvailableTableViewCell.swift
+//  Plentifull
+//
+//  Created by Tracy-ying Zhang on 3/26/18.
+//  Copyright © 2018 Akshay Tracy Co. All rights reserved.
+//
+
+import UIKit
+
+class FoodAvailableTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
